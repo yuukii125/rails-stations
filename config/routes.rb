9426971d6399e930d045f2b5408d1c30ea4937d5
yuukii125 <y.kipro125@gmail.com>
2021-10-17 22:40:28 +0900

    routingの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get "/movies", to: "movies#index"
   namespace :admin do
-    resources :movies, only: [:index]
+    resources :movies, only: [:index, :new, :create]
   end
 end
