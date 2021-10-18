5e37857af7fbbb01d6a6c670f171cd5e4937d601
yuukii125 <y.kipro125@gmail.com>
2021-10-18 23:03:02 +0900

    flashメッセージの作成

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -2,4 +2,25 @@ class Admin::MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.create(movie_params)
+    if @movie.save
+      redirect_to admin_movies_path, notice: "登録しました！"
+    else
+      flash.now[:danger] = "登録に失敗しました。"
+      render action: :new
+    end
+  end
+
+  private
+
+  def movie_params
+    params.permit(:name, :year, :description, :image_url, :is_showing)
+  end
+
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,5 +2,4 @@ class Movie < ApplicationRecord
   validates :name, presence: true, length: { maximum: 160 }, uniqueness: true
   validates :name, presence: true, length: { maximum: 45 }
   validates :image_url, length: { maximum: 45 }
-  validates :is_showing, presence: true
 end
