3e98934399a810fc81f241079fb0fc32c43f260e
yuukii125 <y.kipro125@gmail.com>
2021-10-21 00:38:55 +0900

    validateの変更

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,4 @@
 class Movie < ApplicationRecord
   validates :name, presence: true, length: { maximum: 160 }, uniqueness: true
-  validates :year, presence: true, length: { maximum: 45 }
+  validates :year, length: { maximum: 45 }
 end
