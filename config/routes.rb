871645b34d5999ff1572064cb98b447804b67374
yuukii125 <y.kipro125@gmail.com>
2021-10-20 21:01:11 +0900

    routeの設定と編集ページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get "/movies", to: "movies#index"
   namespace :admin do
-    resources :movies, only: [:index, :new, :create]
+    resources :movies, only: [:index, :new, :create, :edit, :update]
   end
 end
