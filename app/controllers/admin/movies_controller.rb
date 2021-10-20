3374f3e53b4d5211a5c2e745b6ca725c21b08231
yuukii125 <y.kipro125@gmail.com>
2021-10-20 23:21:29 +0900

    privateメソッドの修正

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -35,7 +35,7 @@ class Admin::MoviesController < ApplicationController
   private
 
   def movie_params
-    params.require(:movie).permit(:name, :year, :description, :image_url, :is_showing)
+    params.require(:movie).permit(:id, :name, :year, :description, :image_url, :is_showing)
   end
 
 end
