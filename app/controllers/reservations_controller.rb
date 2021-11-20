16a1441067fa307b4528fa6ef7892f371e2ad2ec
yuukii125 <y.kipro125@gmail.com>
2021-11-20 16:48:22 +0900

    おおまかなreservation/create

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,4 +2,23 @@ class ReservationsController < ApplicationController
   def new
     @reservation = Reservation.new
   end
+
+  def create
+    @reservation = Reservation.create(reservation_params)
+    if @reservation.save
+      # redirect_to movie_path(@reservation.movie_id), notice: "予約しました！"
+      redirect_to movie_path, notice: "予約しました！"
+    else
+      flash.now[:danger] = "予約に失敗しました。"
+      render :new
+    end
+  end
+
+  private
+
+  def reservation_params
+    params.permit(:date, :schedule_id, :sheet_id, :email, :name)
+    # params.require(:reservation).permit(:date, :schedule_id, :sheet_id, :email, :name)
+  end
+
 end
