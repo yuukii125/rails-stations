995fcb97984ec96a193b5e9ea3bcce0d251be201
yuukii125 <y.kipro125@gmail.com>
2021-11-21 23:52:09 +0900

    予約済のときは遷移

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -5,6 +5,11 @@ class ReservationsController < ApplicationController
     @sheet = Sheet.find(params[:sheet_id])
     @reservation = Reservation.new
     @schedule = Schedule.find(params[:schedule_id])
+
+    if (@date.nil?) || (@sheet.nil?)
+      render "sheets/index"
+    end
+
   end
 
   def create
@@ -12,8 +17,18 @@ class ReservationsController < ApplicationController
     @schedule = Schedule.find(params[:schedule_id])
     @sheet = Sheet.find(params[:sheet_id])
     @date = params[:date]
-    @reservation = Reservation.create(reservation_params)
-    if @reservation.save
+    @reservation = Reservation.new(name: reservation_params[:name], email: reservation_params[:email], date: reservation_params[:date], sheet_id: reservation_params[:sheet_id], schedule_id: reservation_params[:schedule_id])
+    @sheets = Sheet.all
+    @already_reservation = Reservation.find_by(date: params[:date], schedule_id: params[:schedule_id], sheet_id: params[:sheet_id])
+
+    if !@already_reservation.nil?
+      flash.now[:danger] = "その座席はすでに予約済です。"
+      @movie
+      @date
+      @schedule
+      @sheets
+      render "sheets/index"
+    elsif @reservation.save
       redirect_to movie_path(@movie.id), notice: "予約しました！"
     else
       flash.now[:danger] = "予約に失敗しました。"
@@ -28,7 +43,7 @@ class ReservationsController < ApplicationController
   private
 
   def reservation_params
-    params.permit(:date, :schedule_id, :sheet_id, :email, :name)
+    params.permit(:date, :schedule_id, :sheet_id, :email, :name, :movie_id)
   end
 
 end
