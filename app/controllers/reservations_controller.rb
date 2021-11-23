f7e217de53cbfb93451a593a7f459fe2143df8b2
yuukii125 <y.kipro125@gmail.com>
2021-11-23 15:03:16 +0900

    重複した記述を統一

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,6 +1,7 @@
 class ReservationsController < ApplicationController
+  before_action :set_movie, only: [:new, :create]
+
   def new
-    @movie = Movie.find(params[:movie_id])
     @date = params[:date]
     @sheet = Sheet.find(params[:sheet_id])
     @reservation = Reservation.new
@@ -13,21 +14,13 @@ class ReservationsController < ApplicationController
   end
 
   def create
-    @movie = Movie.find(params[:movie_id])
-    @schedule = Schedule.find(params[:schedule_id])
-    @sheet = Sheet.find(params[:sheet_id])
-    @date = params[:date]
     @reservation = Reservation.new(name: reservation_params[:name], email: reservation_params[:email], date: reservation_params[:date], sheet_id: reservation_params[:sheet_id], schedule_id: reservation_params[:schedule_id])
     @sheets = Sheet.all
     @already_reservation = Reservation.find_by(date: params[:date], schedule_id: params[:schedule_id], sheet_id: params[:sheet_id])
 
     if !@already_reservation.nil?
-      flash.now[:danger] = "その座席はすでに予約済です。"
-      @movie
-      @date
-      @schedule
       @sheets
-      render "sheets/index"
+      redirect_to movie_schedule_sheets_path(movie_id: params[:movie_id], date: params[:date], schedule_id: params[:schedule_id]), alert: "その座席はすでに予約済です"
     elsif @reservation.save
       redirect_to movie_path(@movie.id), notice: "予約しました！"
     else
@@ -39,5 +32,9 @@ class ReservationsController < ApplicationController
 
   def reservation_params
     params.permit(:date, :schedule_id, :sheet_id, :email, :name, :movie_id)
+  end
+
+  def set_movie
+    @movie = Movie.find(params[:movie_id])
   end
 end
