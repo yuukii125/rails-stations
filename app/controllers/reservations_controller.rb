e650a24836a08fcaf024512d730f8800cc511bdf
yuukii125 <y.kipro125@gmail.com>
2021-12-19 15:06:50 +0900

    station10

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,5 +1,5 @@
 class ReservationsController < ApplicationController
-  before_action :set_movie, only: [:new, :create]
+  # before_action :set_movie, only: [:new, :create]
 
   def new
     if params[:date].present? && params[:sheet_id].present?
@@ -8,8 +8,9 @@ class ReservationsController < ApplicationController
       @date = params[:date].to_date
       @sheet = Sheet.find(params[:sheet_id])
       @schedule = Schedule.find(params[:schedule_id])
+      @movie = Movie.find(params[:movie_id])
     else
-      redirect_to movie_schedule_sheets_path(movie_id: @movie.id, schedule_id: params[:schedule_id]), alert: ""
+      redirect_to movie_schedule_sheets_path(movie_id: params[:movie_id], schedule_id: params[:schedule_id]), alert: ""
     end
   end
 
@@ -19,14 +20,10 @@ class ReservationsController < ApplicationController
                                       date: reservation_params[:date],
                                       sheet_id: reservation_params[:sheet_id],
                                       schedule_id: reservation_params[:schedule_id])
+    @movie = Movie.find(reservation_params[:movie_id])
     if @reservation.save
       redirect_to movie_path(@movie.id), notice: "予約しました！"
     else
-      # redirect_to new_movie_schedule_reservation_path(movie_id: params[:movie_id],
-      #                                                 date: params[:date],
-      #                                                 sheet_id: params[:sheet_id],
-      #                                                 schedule_id: params[:schedule_id]),
-      #                                                 alert: "名前またはメールアドレスを記入してください"
       redirect_to movie_schedule_sheets_path(movie_id: reservation_params[:movie_id],
                                             date: reservation_params[:date],
                                             schedule_id: reservation_params[:schedule_id]),
@@ -41,6 +38,6 @@ class ReservationsController < ApplicationController
   end
 
   def set_movie
-    @movie = Movie.find(params[:movie_id])
+    @movie = Movie.find(reservation_params[:movie_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  resources :reservations, only: [:create]
   resources :movies, only: [:index, :show] do
     resources :schedules, only: [:show] do
       resources :sheets, only: [:index]
-      resources :reservations, only: [:new, :create]
+      resources :reservations, only: [:new]
     end
   end
   resources :sheets, only: [:index]
