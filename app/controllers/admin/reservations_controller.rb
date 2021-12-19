2c0ee75d4c95562f2625d9d1a0870e226ee63f17
yuukii125 <y.kipro125@gmail.com>
2021-12-19 21:54:35 +0900

    destroyの作成

--- app/controllers/admin/reservations_controller.rb
+++ app/controllers/admin/reservations_controller.rb
@@ -5,7 +5,27 @@ class Admin::ReservationsController < ApplicationController
 
   def show
     @reservation = Reservation.find(params[:id])
+    @movie = @reservation.schedule.movie
+    @sheet = @reservation.sheet
+    @schedule = @reservation.schedule
   end
 
+  def new
+    @reservation = Reservation.new
+  end
+
+  def edit
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def destroy
+    @reservation = Reservation.find(params[:id])
+    if @reservation.destroy
+      redirect_to admin_reservations_path, notice: "削除しました！"
+    else
+      flash.now[:danger] = "削除に失敗しました。"
+      render :edit
+    end
+  end
 
 end
